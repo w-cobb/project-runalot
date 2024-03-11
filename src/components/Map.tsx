47de9f901858c64b5a2c8c00d6d05bc63cef347b
William Cobb <william.a.cobb-1@ou.edu>
2024-03-11 16:41:29 -0700

    Modify map center

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap, Polyline } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const MapComponent = () => {
-    const map = useMap();
-    console.log("Map created");
-    return null;
-}
-
 const Map = () => {
-  return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100vh', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
-  );
+    return (
+        <MapContainer center={[47.67, -122.12]} zoom={10} style={{ height: '100vh', width: '100%' }}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+        </MapContainer>
+    );
 };
 
 export default Map;
