206f767186a94feb1fe36420a4cd98b8ddcc57d3
w-cobb <williamcobb930@gmail.com>
2024-03-12 11:20:45 -0700

    Update map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap, Polyline } from 'react-leaflet';
+import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import '../styles/map.scss';
 
 const Map = () => {
     return (
-        <MapContainer center={[47.67, -122.12]} zoom={10} style={{ height: '100vh', width: '100%' }}>
+        <MapContainer center={[47.67, -122.12]} zoom={10} zoomControl={false} className='map'>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-
+            <ZoomControl position='topright'/>
         </MapContainer>
     );
 };
